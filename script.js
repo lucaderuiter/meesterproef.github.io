c0041c9260b13b8411a7d5279322d516e76b62b5
lucaderuiter <lucaderuiter@outlook.com>
2019-06-19 22:09:18 +0200

    upload1

---
+++ script.js
@@ -0,0 +1,234 @@
+
+
+//Alex Abdalla
+function showWater() {
+    var water = document.getElementById("water2");
+    
+    
+    water.style.opacity = "1";
+    water.style.transition = "opacity 1s";
+}
+
+//petitie.addEventListener('click', function() {
+    
+//    water.classList.toggle('.water');
+//});
+
+
+//https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+function myFunction2() {
+    var antw = document.getElementById("antw");
+    
+    antw.style.display = "none";
+    
+    var antwoord1vervolg = document.getElementById("antwoord1vervolg");
+    
+    antwoord1vervolg.style.display = "block";
+}
+
+function myFunction3() {
+    var antw = document.getElementById("antw");
+    antw.style.display = "none";
+    
+    var antwoord2vervolg = document.getElementById("antwoord2vervolg");
+    
+    antwoord2vervolg.style.display = "block";
+}
+
+function funcdisp() {
+    var antw = document.getElementById("antw");
+    
+    antw.style.display = "block";
+    
+    var antwoord1vervolg = document.getElementById("antwoord1vervolg");
+    
+    antwoord1vervolg.style.display = "none";
+    
+    var antwoord2vervolg = document.getElementById("antwoord2vervolg");
+    
+    antwoord2vervolg.style.display = "none";
+}
+
+
+
+function myFunction22() {
+    var antw = document.getElementById("antw-2");
+    
+    antw.style.display = "none";
+    
+    var antwoord1vervolg = document.getElementById("antwoord1vervolg-2");
+    
+    antwoord1vervolg.style.display = "block";
+}
+
+function myFunction33() {
+    var antw = document.getElementById("antw-2");
+    antw.style.display = "none";
+    
+    var antwoord2vervolg = document.getElementById("antwoord2vervolg-2");
+    
+    antwoord2vervolg.style.display = "block";
+}
+
+function funcdisp2() {
+    var antw = document.getElementById("antw-2");
+    
+    antw.style.display = "block";
+    
+    var antwoord1vervolg = document.getElementById("antwoord1vervolg-2");
+    
+    antwoord1vervolg.style.display = "none";
+    
+    var antwoord2vervolg = document.getElementById("antwoord2vervolg-2");
+    
+    antwoord2vervolg.style.display = "none";
+}
+
+
+
+function myFunction222() {
+    var antw = document.getElementById("antw-3");
+    
+    antw.style.display = "none";
+    
+    var antwoord1vervolg = document.getElementById("antwoord1vervolg-3");
+    
+    antwoord1vervolg.style.display = "block";
+}
+
+function myFunction333() {
+    var antw = document.getElementById("antw-3");
+    antw.style.display = "none";
+    
+    var antwoord2vervolg = document.getElementById("antwoord2vervolg-3");
+    
+    antwoord2vervolg.style.display = "block";
+}
+
+function funcdisp3() {
+    var antw = document.getElementById("antw-3");
+    
+    antw.style.display = "block";
+    
+    var antwoord1vervolg = document.getElementById("antwoord1vervolg-3");
+    
+    antwoord1vervolg.style.display = "none";
+    
+    var antwoord2vervolg = document.getElementById("antwoord2vervolg-3");
+    
+    antwoord2vervolg.style.display = "none";
+}
+
+
+
+
+function myFunction2222() {
+    var antw = document.getElementById("antw-4");
+    
+    antw.style.display = "none";
+    
+    var antwoord1vervolg = document.getElementById("antwoord1vervolg-4");
+    
+    antwoord1vervolg.style.display = "block";
+}
+
+
+function myFunction3333() {
+    var antw = document.getElementById("antw-4");
+    antw.style.display = "none";
+    
+    var antwoord2vervolg = document.getElementById("antwoord2vervolg-4");
+    
+    antwoord2vervolg.style.display = "block";
+}
+
+function funcdisp4() {
+    var antw = document.getElementById("antw-4");
+    
+    antw.style.display = "block";
+    
+    var antwoord1vervolg = document.getElementById("antwoord1vervolg-4");
+    
+    antwoord1vervolg.style.display = "none";
+    
+    var antwoord2vervolg = document.getElementById("antwoord2vervolg-4");
+    
+    antwoord2vervolg.style.display = "none";
+}
+
+
+
+function myFunction22222() {
+    var antw = document.getElementById("antw-5");
+    
+    antw.style.display = "none";
+    
+    var antwoord1vervolg = document.getElementById("antwoord1vervolg-5");
+    
+    antwoord1vervolg.style.display = "block";
+}
+
+
+function myFunction33333() {
+    var antw = document.getElementById("antw-5");
+    antw.style.display = "none";
+    
+    var antwoord2vervolg = document.getElementById("antwoord2vervolg-5");
+    
+    antwoord2vervolg.style.display = "block";
+}
+
+function funcdisp5() {
+    var antw = document.getElementById("antw-5");
+    
+    antw.style.display = "block";
+    
+    var antwoord1vervolg = document.getElementById("antwoord1vervolg-5");
+    
+    antwoord1vervolg.style.display = "none";
+    
+    var antwoord2vervolg = document.getElementById("antwoord2vervolg-5");
+    
+    antwoord2vervolg.style.display = "none";
+}
+
+
+function myFunction222222() {
+    var antw = document.getElementById("antw-6");
+    
+    antw.style.display = "none";
+    
+    var antwoord1vervolg = document.getElementById("antwoord1vervolg-6");
+    
+    antwoord1vervolg.style.display = "block";
+}
+
+
+function myFunction333333() {
+    var antw = document.getElementById("antw-6");
+    antw.style.display = "none";
+    
+    var antwoord2vervolg = document.getElementById("antwoord2vervolg-6");
+    
+    antwoord2vervolg.style.display = "block";
+}
+
+function funcdisp6() {
+    var antw = document.getElementById("antw-6");
+    
+    antw.style.display = "block";
+    
+    var antwoord1vervolg = document.getElementById("antwoord1vervolg-6");
+    
+    antwoord1vervolg.style.display = "none";
+    
+    var antwoord2vervolg = document.getElementById("antwoord2vervolg-6");
+    
+    antwoord2vervolg.style.display = "none";
+}
+
+
+
+
+
+
